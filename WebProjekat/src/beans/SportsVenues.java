b8af5d5a1dc21932e708a6f215db21d82965c306
DaniloBulatovic <93370954+DaniloBulatovic@users.noreply.github.com>
2022-06-18 16:15:11 +0200

    Added filtering and sorting to venues table

--- WebProjekat/src/beans/SportsVenues.java
+++ WebProjekat/src/beans/SportsVenues.java
@@ -4,7 +4,15 @@ import java.lang.reflect.Type;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.LinkedHashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
 
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
@@ -21,11 +29,32 @@ public class SportsVenues {
 		}
 	}
 	
+	private static HashMap<String, SportsVenue> sortValues(HashMap<String, SportsVenue> map)   
+	{   
+		List<Map.Entry<String, SportsVenue>> list = new LinkedList<Map.Entry<String, SportsVenue>>(map.entrySet());  
+		//Custom Comparator  
+		Collections.sort(list, new Comparator() {  
+			public int compare(Object o1, Object o2)   
+			{  
+				return (-1 * Boolean.compare(((Map.Entry<String, SportsVenue>) o1).getValue().isWorking(),((Map.Entry<String, SportsVenue>) (o2)).getValue().isWorking()));  
+			}  
+		});  
+		//copying the sorted list in HashMap to preserve the iteration order  
+		HashMap<String, SportsVenue> sortedHashMap = new LinkedHashMap<String, SportsVenue>();  
+		for (Iterator<Entry<String, SportsVenue>> it = list.iterator(); it.hasNext();)   
+		{  
+			 Map.Entry<String, SportsVenue> entry = (Map.Entry<String, SportsVenue>) it.next();  
+			 sortedHashMap.put(entry.getKey(), entry.getValue());  
+		}   
+		return sortedHashMap;  
+	}  
+	
 	public void readSportVenues() throws Exception
     {
 		String json = new String(Files.readAllBytes(Paths.get("./static/data/sportsVenues.json")));
 		Type type = new TypeToken<HashMap<String, SportsVenue>>(){}.getType();
 		sportsVenues = new Gson().fromJson(json, type);
+		sportsVenues = sortValues(sportsVenues);
     }
 
 	public Collection<SportsVenue> getValues() {
--- WebProjekat/static/components/venues.js
+++ WebProjekat/static/components/venues.js
@@ -2,20 +2,29 @@ Vue.component("venues", {
 	data: function () {
 	    return {
 		  search: "",
+		  filterWorking: false,
 	      venues: null
 	    }
 	},
 	    template: ` 
     	<div>
     		<h2>Prikaz sportskih objekata</h2>
-    		<p id="searchParagraph"><input type="text" v-model="search" placeholder="Pretraga objekata.."></p>
-    		<table class="venues_table">
+    		<p id="searchParagraph">
+    			<label>Tip</label>
+    			<select id="typeDropdown" v-on:change="filterTableByType()">
+    			 	<option>Sve</option>
+				    <option v-for="(v, index) in filteredVenues">{{v.venueType}}</option>
+				</select>
+				<input type="checkbox" v-model="filterWorking">Prikaži samo otvorene
+    			<input type="text" v-model="search" placeholder="Pretraga objekata..">
+    		</p>
+    		<table id="venues_table" class="venues_table">
 	    		<tr>
 	    			<th>Logo</th>
-	    			<th>Naziv</th>
+	    			<th v-on:click="sortTable(1)" style="cursor:pointer">Naziv</th>
 	    			<th>Tip</th>
-	    			<th>Lokacija</th>
-	    			<th>Prosečna ocena</th>
+	    			<th v-on:click="sortTable(3)" style="cursor:pointer">Lokacija</th>
+	    			<th v-on:click="sortTable(4)" style="cursor:pointer">Prosečna ocena</th>
 	    			<th>Radno vreme</th>
 	    			<!--<th>Akcija</th>-->
 	    		</tr>
@@ -54,13 +63,74 @@ Vue.component("venues", {
 	            .delete('rest/venues/delete/' + id)
 	            .then(response => (this.venues.splice(index, 1)))
     		}
-    	}
+    	},
+    	sortTable(n) {
+			  var table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
+			  table = document.getElementById("venues_table");
+			  switching = true;
+			  dir = "asc";
+			  while (switching) {
+			    switching = false;
+			    rows = table.rows;
+			    for (i = 1; i < (rows.length - 1); i++) {
+			      shouldSwitch = false;
+			      x = rows[i].getElementsByTagName("TD")[n];
+			      y = rows[i + 1].getElementsByTagName("TD")[n];
+			      if (dir == "asc") {
+			        if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+			          shouldSwitch = true;
+			          break;
+			        }
+			      } else if (dir == "desc") {
+			        if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
+			          shouldSwitch = true;
+			          break;
+			        }
+			      }
+			    }
+			    if (shouldSwitch) {
+			      rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+			      switching = true;
+			      switchcount ++;
+			    } else {
+			      if (switchcount == 0 && dir == "asc") {
+			        dir = "desc";
+			        switching = true;
+			      }
+			    }
+			}
+		},
+		filterTableByType() {
+		  let dropdown, table, rows, cells, type, filter;
+		  dropdown = document.getElementById("typeDropdown");
+		  table = document.getElementById("venues_table");
+		  rows = table.getElementsByTagName("tr");
+		  filter = dropdown.value;
+		
+		  for (let row of rows) {
+		    cells = row.getElementsByTagName("td");
+		    type = cells[2] || null;
+		    if (filter === "Sve" || !type || (filter === type.textContent)) {
+		      row.style.display = "";
+		    }
+		    else {
+		      row.style.display = "none";
+		    }
+		  }
+		}
     },
     computed: {
 		filteredVenues() {
 			if (this.venues == null){ 
 				axios.get('rest/venues/').then(response => (this.venues = response.data));
 				return this.venues;
+			}
+			else if (this.filterWorking == true){
+				return this.venues.filter(venue => {
+    	     			return (venue.isWorking == true) && ((venue.name.toLowerCase().indexOf(this.search.toLowerCase()) > -1)
+    	     			 || (venue.venueType.toLowerCase().indexOf(this.search.toLowerCase()) > -1)
+    	     			 || (venue.location.address.toLowerCase().indexOf(this.search.toLowerCase()) > -1)
+    	     			 || (venue.averageGrade.toString().indexOf(this.search) > -1))})
 			}
 			else{
 				return this.venues.filter(venue => {
