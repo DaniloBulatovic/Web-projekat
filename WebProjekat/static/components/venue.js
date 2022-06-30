cd9cba11fafaa996ed10753ac4cd152bf8cb1921
DaniloBulatovic <bulatovic.ra159.2019@uns.ac.rs>
2022-06-30 15:48:28 +0200

    Added map location display for selected venue

--- WebProjekat/static/components/venue.js
+++ WebProjekat/static/components/venue.js
@@ -4,7 +4,7 @@ Vue.component("venue", {
 		    return {
 		      id : 0,
 			  visible : false,
-		      venue: {id: '', name:null, venueType:0, content:null, isWorking:true, location:null, logoPath:null, averageGrade:null, workingHours:null}
+		      venue: {id: '', name:null, venueType:0, content:null, isWorking:true, location:{ latitude:null, longitude:null, address:{ street:null, number:null, city:null, country:null, postalCode:null}}, logoPath:null, averageGrade:null, workingHours:null}
 		    }
 	},
 	template: ` 
@@ -34,7 +34,9 @@ Vue.component("venue", {
 													+ venue.location.address.number + ', '
 													+ venue.location.address.city + ', ' 
 													+ venue.location.address.postalCode" readonly></td>
-			</tr
+			</tr>
+			<tr>
+				<th colspan=2><div ref="map-root" id="map" class="map" style="height:200px; width:100%"></div></th>
 			</tr>
 			<tr>
 				<td>Prosečna ocena</td>
@@ -64,7 +66,40 @@ Vue.component("venue", {
 			this.visible = true;
 	        axios
 	          .get('rest/venues/' + this.id)
-	          .then(response => (this.venue = response.data))
+	          .then(response => {
+				this.venue = response.data;
+				
+				const iconFeature = new ol.Feature({
+				  geometry: new ol.geom.Point(ol.proj.fromLonLat([this.venue.location.longitude, this.venue.location.latitude])),
+				  name: this.venue.name
+				});
+				
+				var map = new ol.Map({
+				  layers: [
+				    new ol.layer.Tile({source: new ol.source.OSM()}),
+					new ol.layer.Vector({
+					      source: new ol.source.Vector({
+					        features: [iconFeature]
+					      }),
+					      style: new ol.style.Style({
+					        image: new ol.style.Icon({
+							  scale: 0.05,
+					          anchor: [0.5, 460],
+					          anchorXUnits: 'fraction',
+					          anchorYUnits: 'pixels',
+					          src: './images/icons/location.png'
+					        })
+					      })
+					    })
+				  ],
+				  view: new ol.View({
+				    center: ol.proj.fromLonLat([this.venue.location.longitude, this.venue.location.latitude]),
+				    zoom: 15,
+					constrainResolution: true
+				  }),
+				  target: this.$refs['map-root']
+				});
+		})
 		}else{
 			this.visible = false;
 		}
